cfca39886c41284a011ec898435af8a0748baad1
HaHARSHAL-AP <apsundeharshal129@gmail.com>
2022-09-28 01:55:09 +0530

    homepage

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,12 +1,35 @@
 import Navbar  from "../components/Navbar.jsx";
-
+import { Container, Box, Image,Flex,Heading ,Spacer,ButtonGroup,Button,Center,Text} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 function Home(){
 
     return (
         <div>
          <Navbar/>
-        <h1>Home</h1>
+         <Container maxW="100%" bg="white" h="800px" centerContent pt="50px">
+         <Flex  h="550px" >
+         <Box w='600px'  textAlign="left">
+           <Text fontSize='50px ' as="b"  color="black">Eat smarte, </Text><br/>
+           <Text fontSize='50px' as="b" color="black"> Live better</Text>
+           <Text color="black" fontSize='30px'>Track your calories, exercise, </Text>
+           <Text color="black" fontSize='30px'> biometrics and health data.</Text>
+           <Button bg="tomato" fontSize='20px' mt="50px" w="300px" h="50px"><Link to="/signup">Sign UP-It's Free!</Link></Button>
+           <Text color="black" mt="30px">Already have an account? <span ><Link to="/login">Log in.</Link></span></Text>
+         </Box>
+         
+         
+         <Box w='50%' >
+    
+           <Image src='https://cdn1.cronometer.com/2021/landing/cronometer-hero-graphic.svg' height="500px" maxW="600px" alt='Dan Abramov' />
+          
+         </Box>
+         
+
+         </Flex>
+         </Container>
+
+
         </div>
         
     )
