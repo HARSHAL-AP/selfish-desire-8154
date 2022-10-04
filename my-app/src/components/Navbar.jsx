224e0dcd085ac9bc07a042f81f6ad4f2454b5ca4
HaHARSHAL-AP <apsundeharshal129@gmail.com>
2022-10-04 23:19:14 +0530

    about page

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -3,24 +3,24 @@ import {Link} from "react-router-dom"
 
 function Navbar() {
   return (
-    <Container maxW="100%" bg="white" height="100px" centerContent>
-      <Container h="60px" m="auto" maxW="6xl" display="flex">
+    <Container maxW="100%" bg="white" height="100px" >
+      <Container h="60px" m="auto" maxW="75%" display="flex">
         <Image
           src="https://cdn1.cronometer.com/2021/landing/cronometer-logo.svg"
-          w="250px"
+          maxW="20%"
           alt="logo"
         />
 
-<Flex minWidth='max-content' alignItems='center' gap='2'>
+<Flex maxW='800px' alignItems='center' gap='2' >
   
   <Spacer />
   <ButtonGroup gap='2' pl="250px">
-    <Button color="black" bg="white" fontSize={17}><Link to="/product">Product</Link></Button>
-    <Button color="black" bg="white" fontSize={17}><Link >Support</Link></Button>
-    <Button color="black" bg="white" fontSize={17}><Link > Blog</Link></Button>
-    <Button color="black" bg="white" fontSize={17}><Link >Forum</Link></Button>
-    <Button color="black" bg="white" fontSize={17}><Link to="/aboute">Aboute</Link></Button>
-    <Button colorScheme='white' boxShadow='dark-lg' w="100px" border="1px" borderColor="tomato" color="tomato"><Link to="/login">Login</Link></Button>
+    <Button color="black" bg="white" maxW="100px"  fontSize={17}><Link to="/product">Product</Link></Button>
+    <Button color="black" bg="white" maxW="100px" fontSize={17}><Link >Support</Link></Button>
+    <Button color="black" bg="white" maxW="100px" fontSize={17}><Link > Blog</Link></Button>
+    <Button color="black" bg="white" maxW="100px" fontSize={17}><Link >Forum</Link></Button>
+    <Button color="black" bg="white" maxW="100px" fontSize={17}><Link to="/aboute">Aboute</Link></Button>
+    <Button colorScheme='white' boxShadow='dark-lg'  border="1px" borderColor="tomato" color="tomato"><Link to="/login">Login</Link></Button>
 
   </ButtonGroup>
 </Flex>
