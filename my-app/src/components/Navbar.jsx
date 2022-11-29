324ef6581371c409d605546aeef6f2a660b97808
HARSHAL-AP <apsundeharshal129@gmail.com>
2022-11-29 23:40:05 +0530

    start making responsive

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,31 +1,55 @@
-import { Container, Box, Image,Flex,Heading ,Spacer,ButtonGroup,Button} from "@chakra-ui/react";
-import {Link} from "react-router-dom"
+import { Container, Box, Image, Flex, Heading, Spacer, ButtonGroup, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom"
+import style from "./Navbar.css"
+
 
 function Navbar() {
   return (
-    <Container maxW="100%" bg="white" height="100px" >
-      <Container h="60px" m="auto" maxW="75%" display="flex">
-        <Image
+    <>
+    <div className="navbar">
+     <div className="navbody">
+       <div className="logo">
+       <Image
           src="https://cdn1.cronometer.com/2021/landing/cronometer-logo.svg"
-          maxW="20%"
+          W="80%"
           alt="logo"
         />
+       </div>
+       <div className="routbody">
+        <ButtonGroup gap='2' pl="250px">
+            <Button color="black" bg="white" maxW="100px" fontSize={17}><Link to="/product">Product</Link></Button>
+            <Button color="black" bg="white" maxW="100px" fontSize={17}><Link >Support</Link></Button>
+            <Button color="black" bg="white" maxW="100px" fontSize={17}><Link > Blog</Link></Button>
+            <Button color="black" bg="white" maxW="100px" fontSize={17}><Link >Forum</Link></Button>
+            <Button color="black" bg="white" maxW="100px" fontSize={17}><Link to="/aboute">About</Link></Button>
+            <Button colorScheme='white'  boxShadow='dark-lg' border="1px" borderColor="tomato" color="tomato"><Link to="/login">Login</Link></Button>
 
-<Flex maxW='800px' alignItems='center' gap='2' >
-  
-  <Spacer />
-  <ButtonGroup gap='2' pl="250px">
-    <Button color="black" bg="white" maxW="100px"  fontSize={17}><Link to="/product">Product</Link></Button>
-    <Button color="black" bg="white" maxW="100px" fontSize={17}><Link >Support</Link></Button>
-    <Button color="black" bg="white" maxW="100px" fontSize={17}><Link > Blog</Link></Button>
-    <Button color="black" bg="white" maxW="100px" fontSize={17}><Link >Forum</Link></Button>
-    <Button color="black" bg="white" maxW="100px" fontSize={17}><Link to="/aboute">Aboute</Link></Button>
-    <Button colorScheme='white' boxShadow='dark-lg'  border="1px" borderColor="tomato" color="tomato"><Link to="/login">Login</Link></Button>
+        </ButtonGroup>
+       </div>
+       <div className="hambur">
+       <Button color="black"  bg="red.300" fontSize={20}><Link >=</Link></Button>
+       </div>
+     </div>
 
-  </ButtonGroup>
-</Flex>
-      </Container>
-    </Container>
+
+
+
+    </div>
+    <div className="hamburgermenu">
+     <div className="hamtop">
+     <Button color="black"  bg="red.300" fontSize={20}><Link >clear</Link></Button>
+     </div>
+     <div>
+     <div><Link to="/product">Product</Link></div>
+     <div><Link >Support</Link></div>
+     <div><Link > Blog</Link></div>
+     <div><Link >Forum</Link></div>
+     <div><Link to="/aboute">About</Link></div>
+     </div>
+     
+    </div>
+    </>
+    
   );
 }
 export default Navbar;
